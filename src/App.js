1c08bb66c859a31dbd6f63864d5024a9827494ee
Lali <lauradiaz1586@gmail.com>
2021-02-20 14:56:18 +0400

    add project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Modal from './components/Modal';
+import Sidebar from './components/Sidebar';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <>
+      <h2>modal-sidebar project setup</h2>
+    </>
+  );
+}
+
+export default App;
